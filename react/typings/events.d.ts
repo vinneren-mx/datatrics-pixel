cf3c0e082d5dd807d8de74dc7bf962efab470610
Ernesto Rodríguez <ernesto@vinneren.com.mx>
2022-05-22 23:28:06 -0500

    mas lint

--- react/typings/events.d.ts
+++ react/typings/events.d.ts
@@ -10,191 +10,191 @@ export interface PixelMessage extends MessageEvent {
     | CategoryViewData
     | DepartmentViewData
     | CartChangedData
-    | UserData;
+    | UserData
 }
 
 export interface EventData {
-  event: string;
-  eventName: string;
-  currency: string;
+  event: string
+  eventName: string
+  currency: string
 }
 
 export interface PageViewData extends EventData {
-  event: 'pageView';
-  eventName: 'vtex:pageView';
-  pageTitle: string;
-  pageUrl: string;
-  referrer: string;
+  event: 'pageView'
+  eventName: 'vtex:pageView'
+  pageTitle: string
+  pageUrl: string
+  referrer: string
 }
 
 export interface CategoryViewData extends EventData {
-  event: 'categoryView';
-  eventName: 'vtex:categoryView';
-  products: Product[];
+  event: 'categoryView'
+  eventName: 'vtex:categoryView'
+  products: Product[]
 }
 
 export interface DepartmentViewData extends EventData {
-  event: 'departmentView';
-  eventName: 'vtex:departmentView';
-  products: Product[];
+  event: 'departmentView'
+  eventName: 'vtex:departmentView'
+  products: Product[]
 }
 
 export interface AddToCartData extends EventData {
-  event: 'addToCart';
-  eventName: 'vtex:addToCart';
-  items: Item[];
+  event: 'addToCart'
+  eventName: 'vtex:addToCart'
+  items: Item[]
 }
 
 export interface RemoveToCartData extends Order, EventData {
-  event: 'removeFromCart';
-  eventName: 'vtex:removeFromCart';
-  items: Item[];
+  event: 'removeFromCart'
+  eventName: 'vtex:removeFromCart'
+  items: Item[]
 }
 
 export interface CartChangedData extends Order, EventData {
-  event: 'cartChanged';
-  eventName: 'vtex:cartChanged';
-  items: Item[];
+  event: 'cartChanged'
+  eventName: 'vtex:cartChanged'
+  items: Item[]
 }
 
 export interface OrderPlacedData extends Order, EventData {
-  event: 'orderPlaced';
-  eventName: 'vtex:orderPlaced';
+  event: 'orderPlaced'
+  eventName: 'vtex:orderPlaced'
 }
 
 export interface ProductViewData extends EventData {
-  event: 'productView';
-  eventName: 'vtex:productView';
-  product: Product;
+  event: 'productView'
+  eventName: 'vtex:productView'
+  product: Product
 }
 
 export interface ProductClickData extends EventData {
-  event: 'productClick';
-  eventName: 'vtex:productClick';
-  product: Product;
+  event: 'productClick'
+  eventName: 'vtex:productClick'
+  product: Product
 }
 
 export interface ProductImpressionData extends EventData {
-  event: 'productImpression';
-  eventName: 'vtex:productImpression';
-  product: Product;
-  position: number;
-  list: string;
+  event: 'productImpression'
+  eventName: 'vtex:productImpression'
+  product: Product
+  position: number
+  list: string
 }
 
 export interface UserData extends PageInfoData {
-  eventType: 'userData';
-  eventName: 'vtex:userData';
-  firstName?: string;
-  lastName?: string;
-  document?: string;
-  id?: string;
-  email?: string;
-  phone?: string;
-  isAuthenticated: boolean;
+  eventType: 'userData'
+  eventName: 'vtex:userData'
+  firstName?: string
+  lastName?: string
+  document?: string
+  id?: string
+  email?: string
+  phone?: string
+  isAuthenticated: boolean
 }
 
 export interface Order {
-  currency: string;
-  accountName: string;
-  orderGroup: string;
-  salesChannel: string;
-  coupon: string;
-  visitorType: string;
-  visitorContactInfo: string[];
-  transactionId: string;
-  transactionDate: string;
-  transactionAffiliation: string;
-  transactionTotal: number;
-  transactionShipping: number;
-  transactionDiscounts: number;
-  transactionTax: number;
-  transactionCurrency: string;
-  transactionPaymentType: PaymentType[];
-  transactionShippingMethod: ShippingMethod[];
-  transactionProducts: ProductOrder[];
+  currency: string
+  accountName: string
+  orderGroup: string
+  salesChannel: string
+  coupon: string
+  visitorType: string
+  visitorContactInfo: string[]
+  transactionId: string
+  transactionDate: string
+  transactionAffiliation: string
+  transactionTotal: number
+  transactionShipping: number
+  transactionDiscounts: number
+  transactionTax: number
+  transactionCurrency: string
+  transactionPaymentType: PaymentType[]
+  transactionShippingMethod: ShippingMethod[]
+  transactionProducts: ProductOrder[]
   transactionPayment: {
-    id: string;
-  };
+    id: string
+  }
 }
 
 interface PaymentType {
-  group: string;
-  paymentSystemName: string;
-  installments: number;
-  value: number;
+  group: string
+  paymentSystemName: string
+  installments: number
+  value: number
 }
 
 interface ShippingMethod {
-  itemId: string;
-  selectedSla: string;
+  itemId: string
+  selectedSla: string
 }
 
 interface ProductOrder {
-  id: string;
-  name: string;
-  sku: string;
-  skuRefId: string;
-  skuName: string;
-  brand: string;
-  brandId: string;
-  seller: string;
-  sellerId: string;
-  category: string;
-  categoryId: string;
-  categoryTree: string[];
-  categoryIdTree: string[];
-  originalPrice: number;
-  price: number;
-  sellingPrice: number;
-  tax: number;
-  quantity: number;
-  components: any[];
-  measurementUnit: string;
-  unitMultiplier: number;
+  id: string
+  name: string
+  sku: string
+  skuRefId: string
+  skuName: string
+  brand: string
+  brandId: string
+  seller: string
+  sellerId: string
+  category: string
+  categoryId: string
+  categoryTree: string[]
+  categoryIdTree: string[]
+  originalPrice: number
+  price: number
+  sellingPrice: number
+  tax: number
+  quantity: number
+  components: any[]
+  measurementUnit: string
+  unitMultiplier: number
 }
 
 export interface Product {
-  brand: string;
-  categoryId?: string; // inconsistency
-  categories: string[];
-  productId: string;
-  productName: string;
-  selectedSku: Item; // inconsistency
-  items: Item[];
-  sku: Item;
-  [key: string]: any;
+  brand: string
+  categoryId?: string // inconsistency
+  categories: string[]
+  productId: string
+  productName: string
+  selectedSku: Item // inconsistency
+  items: Item[]
+  sku: Item
+  [key: string]: any
 }
 
 interface Item {
-  ean: string;
-  itemId: string;
-  name: string;
-  seller: Seller;
-  sellers: Seller[];
-  [key: string]: any;
+  ean: string
+  itemId: string
+  name: string
+  seller: Seller
+  sellers: Seller[]
+  [key: string]: any
 }
 
 interface Seller {
-  commertialOffer: CommertialOffer;
-  AvailableQuantity: number;
-  [key: string]: any;
+  commertialOffer: CommertialOffer
+  AvailableQuantity: number
+  [key: string]: any
 }
 
 interface CommertialOffer {
-  Price: number;
-  [key: string]: any;
+  Price: number
+  [key: string]: any
 }
 
 interface AddToCartProduct {
-  brand: string;
-  name: string;
-  price: number;
-  ean: string;
-  quantity: number;
-  referenceId: any[];
-  skuId: string;
-  variant: string;
-  category: string;
-  [key: string]: any;
+  brand: string
+  name: string
+  price: number
+  ean: string
+  quantity: number
+  referenceId: any[]
+  skuId: string
+  variant: string
+  category: string
+  [key: string]: any
 }
